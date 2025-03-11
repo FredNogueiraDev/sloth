880d0230e362ef2563aa956a6ee082b341298ed0
Frederico Nogueira <fredericonogueira01@outlook.com>
2025-03-11 07:19:58 -0300

    Estilização 'R$' no input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,15 +7,20 @@ interface InputProps extends ComponentProps<'input'> {
 
 export function Input ({id, title, ...props }: InputProps) {
   return (
-    <div className="flex flex-col w-[48%]">
+    <div className="flex flex-col w-[48%] ">
       <label className="font-medium text-green-light" htmlFor={id}>{title}</label>
-      <input
-      {...props}
-      id={id}
-      className="rounded-lg border border-black-300 px-3 h-[48px]
-      placeholder:text-black-300 font-medium text-green-darker hover:border-green-normalActive
-      focus:outline focus:-outline-offset-2 outline:border-green-normalActive mb-4"
-      />
+
+      <div className="flex max-w-[100%] items-center relative mt-1">
+        <span className="absolute fill-none w-6 text-green-darker font-medium left-4 mb-4">R$</span>
+        <input
+        {...props}
+        id={id}
+        className="rounded-lg border border-black-300 px-3 h-[48px] pl-12
+        placeholder:text-black-300 font-medium text-green-darker hover:border-green-normalActive
+        focus:outline focus:-outline-offset-2 outline:border-green-normalActive mb-4 max-w-full"
+        />
+      </div>
+
     </div>
   )
 }
